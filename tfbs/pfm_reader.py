27a4832a735684b49dae400a9e824c4b00987cac
malcolm <mgperry32@gmail.com>
2021-09-28 16:11:55 +0100

    add PFM class + pydantic for data validation

--- tfbs/pfm_reader.py
+++ tfbs/pfm_reader.py
@@ -1,19 +1,37 @@
-import sys
-import json
 import re
+import pydantic
+from typing import List
 from itertools import zip_longest
+
+class PFM(pydantic.BaseModel):
+
+    id: str
+    metadata = {}
+    PFM: List[List[int]]
+
+    @pydantic.validator("PFM")
+    @classmethod
+    def validate_PFM(cls, rows):
+        
+        if len(rows) != 4:
+            raise ValueError("PFM should have 4 rows")
+
+        if len(set(len(row) for row in rows)) > 1:
+            raise ValueError("All PFM rows should be the same length")
+        
+        return rows
 
 
 def pfm_reader(f, delimiter="\s+"):
     for lines in grouper(5, f):
         header = lines[0].rstrip()
-        if header[0] == ">":
+        if header.startswith(">"):
             header = header[1:]
         info = re.split(delimiter, header)
 
         matrix = [strip(re.split(delimiter, l)) for l in lines[1:]]
 
-        yield {"info": info, "PFM": matrix}
+        yield info, matrix
 
 
 def strip(xs):
